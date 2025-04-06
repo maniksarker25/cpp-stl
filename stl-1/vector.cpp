3d501dd663f48346f38de3d5c5234d9b0ce9da60
manik <maniksarker652@gmail.com>
2025-04-06 20:03:15 +0600

    learn about vector functions

---
+++ stl-1/vector.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    // int n;
+    // cin >>n;
+    // vector<int>v(n);
+    // vector<int>v;
+
+
+    // for(int i = 0;i<n;i++){
+    //     // cin >> v[i];
+    //     int x;
+    //     cin >>x;
+    //     v.push_back(x);
+    // }
+
+    // pop back for remove last element
+    // v.pop_back();
+    
+    // for(int i = 0;i<v.size();i++){
+    //     cout << v[i] << ' ';
+    // }
+
+    // print first eliment
+    // cout << v.front() << endl;
+    // last element
+    // cout << v.back() << endl;
+
+
+    // clear vector
+    // v.clear();
+
+    // check vector empty  => if not empty return 0 and if empty return 1
+    // cout << v.empty() << endl;
+
+
+
+    // 
+
+    // int n;
+    // cin >> n;
+
+
+    // vector<int>v(n); // initially it all element is 0 
+    // vector<int>v(n,5) // initially it all element is 5
+
+    // for(int i = 0;i<n;i++){
+    //     cout << v[i] << ' ';
+    // }
+
+    // v.assign(n,5); // assing 5 for all element
+
+    // for(int i = 0;i<v.size();i++){
+    //     cout << v[i] << ' ';
+    // }
+
+
+
+    // resize ----
+
+    // vector<int>r;
+    // r.resize(3);
+
+    // cout << r.size() << endl;
+
+    // for(int i = 0;i<r.size();i++){
+    //     cin >> r[i];
+    // }
+
+    // for(int i = 0;i<r.size();i++){
+    //     cout << r[i] << ' ';
+    // }
+
+
+
+    return 0;
+}
