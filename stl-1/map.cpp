521e262d6d1af08cc79213f7dfd3dd2ad71c4090
manik <maniksarker652@gmail.com>
2025-04-10 20:17:14 +0600

    learn find fun in map

--- stl-1/map.cpp
+++ stl-1/map.cpp
@@ -13,10 +13,13 @@ int main()
 
     // insert into mp
     // mp.insert({2,20});
-    mp[2] = 20;
+    mp[2] = 20;  // 0(log n)
     mp[4] = 30;
     mp[4] =40; // reply 30 to 40 for 4(key)
     mp[1] = 10;
+    mp[6] = 60;
+    mp[7] = 76;
+    mp[77] = 35;
 
     // print cout 
     // cout << mp[2] << ' ' << mp[4] <<endl;
@@ -27,10 +30,30 @@ int main()
     // }
 
     // use itirator
-    for(auto it : mp){
-        int key = it.first, value = it.second;
-        cout << key << ' ' << value << endl;
-    }
+    // for(auto it : mp){
+    //     int key = it.first, value = it.second;
+    //     cout << key << ' ' << value << endl;
+    // }
+
+
+    // find operation --------------------
+    // auto it = mp.find(45);
+    // cout << it->first << ' ' << it->second << endl;
+    // if(it==mp.end()){
+    //     cout << "Not found" << endl;
+    // }
+    // else{
+    //     cout << "Found" << ' ' << it->first << ' ' << it->second << endl;
+    // }
+
+
+    // 
+    // cout << mp[99] << endl; // if not found then it add one key, value in map {99,0} and return 0
+
+
+    
+
+
 
     return 0;
 }
