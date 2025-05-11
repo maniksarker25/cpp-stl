7603a09e00e27178cd38e7aef8d32ee0769eb610
manik <maniksarker652@gmail.com>
2025-05-11 20:51:34 +0600

    leearn check on off bit and print all on of and turn off kth bit

---
+++ bit-manipulation/betwise_operations.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// check k tomo bit on or off
+bool check_kth_bit_on_or_off(int n,int k){
+    return ((n >>k) & 1);
+}
+
+// print on and off bits
+void print_on_and_off_bits(int n){
+    for(int i = 7;i >= 0;i--){
+        if(check_kth_bit_on_or_off(n,i)){
+            cout << 1 << endl;
+        }
+        else{
+            cout << 0 <<endl;
+        }
+
+    }
+}
+
+
+int turn_on_kth_bit(int n , int k){
+    return (n | (1 <<k));
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    // check k tomo bit on or off
+    // cout << check_kth_bit_on_or_off(45,4) << endl;
+
+    // print on and off bits
+    // print_on_and_off_bits(45);
+
+    // turn on kth bit
+    cout << turn_on_kth_bit(45,4) << endl;
+
+    
+    return 0;
+}
