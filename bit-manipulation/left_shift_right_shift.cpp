978eb41c4cff3c683e1f74c966c4120b22ad56fc
manik <maniksarker652@gmail.com>
2025-05-11 19:22:44 +0600

    learn left shif and right shift operator

---
+++ bit-manipulation/left_shift_right_shift.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int n;
+    int leftShift = (11 << 2);  // kon value left shift korta chaii and koto gor left shift korta chai
+    int rightShif = (11 >> 1);
+    cout << leftShift << endl;
+    cout << rightShif << endl;
+
+    
+    return 0;
+}
