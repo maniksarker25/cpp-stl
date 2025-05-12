81ea101ea50115bb5e28ebe7e8902e88b2437024
manik <maniksarker652@gmail.com>
2025-05-12 19:29:11 +0600

    learn built in funtions

---
+++ bit-manipulation/build_in_function.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    // count on bits using build in fun
+    cout << __builtin_popcount(39) << endl;
+
+    cout << __lg(39) << endl;
+
+    return 0;
+}
