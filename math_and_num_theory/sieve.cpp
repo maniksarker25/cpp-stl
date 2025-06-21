6e2bd83b7bc18e721f0454d26dc7469422f85738
manik <maniksarker652@gmail.com>
2025-06-22 00:09:16 +0600

    learn sieve algo for prime

---
+++ math_and_num_theory/sieve.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+  
+    int n;
+    cin >> n;
+    vector<bool>prime(n+1,true);
+
+    for(int i = 2;i*i<=n;i++){
+        if(prime[i]){
+            for(int j = i+i;j<=n;j+=i){
+                prime[j] = false;
+            }
+        }
+    }
+
+    // print all prime
+    for(int i = 2;i<=n;i++){
+        if(prime[i]){
+            cout << i << ' ';
+        }
+    }
+    cout << endl;
+
+    // check print 
+    if(prime[27]){
+        cout << "Prime" << endl;
+    }
+    else{
+        cout << "Not Prime" <<endl;
+    }
+    return 0;
+}
